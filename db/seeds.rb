4561f936466d3863e19f160e8432abcd2b20f490
augusto <augustozab27@gmail.com>
2024-02-21 16:35:50 -0300

    add_flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating restaurants..."
+london = {name: "Light & Spacious Garden Flat London", address: "10 Clifton Gardens London ", description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 75, number_of_guests: 3}
+
+newyork = {name: "Light & Spacious Garden Flat New York", address: "10 Clifton Gardens New York ", description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 75, number_of_guests: 3}
+
+paris = {name: "Light & Spacious Garden Flat Paris", address: "10 Clifton Gardens Paris ", description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 75, number_of_guests: 3}
+
+barcelona = {name: "Light & Spacious Garden Flat Barcelona", address: "10 Clifton Gardens Barcelona ", description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 75, number_of_guests: 3}
+
+
+[london, newyork, paris, barcelona].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts "Finished!"
